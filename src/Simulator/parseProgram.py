8737ef4f20e7e3c0f3d33f49450a3ca299d7916a
Yuheng Yang <yuhengy@mit.edu>
2025-02-23 22:36:05 -0500

    smallfix

--- src/Simulator/parseProgram.py
+++ src/Simulator/parseProgram.py
@@ -5,27 +5,27 @@ def instToStr_noName(inst):
     if inst["dest"]==0:
       return "__"
     else:
-      return f"r{inst["dest"]}"
+      return f"r{inst['dest']}"
   def srcStr():
     if inst["src"]==7:
       return "rSec"
     else:
-      return f"r{inst["src"]}"
+      return f"r{inst['src']}"
 
 
   if   inst["opcode"]=="ALU":
-    return f"{destStr()} <- ALU-{inst["port"]}({srcStr()}, lat={inst["latency"]})"
+    return f"{destStr()} <- ALU-{inst['port']}({srcStr()}, lat={inst['latency']})"
 
 
   elif inst["opcode"]=="LOAD":
     if "src" in inst:
       return f"{destStr()} <- LOAD({srcStr()})"
     else:
-      return f"{destStr()} <- LOAD(0x{inst["srcImm"]})"
+      return f"{destStr()} <- LOAD(0x{inst['srcImm']})"
 
 
   elif inst["opcode"]=="BREZ":
-    return f"IF ({srcStr()}==0) PC += {inst["offset"]}"
+    return f"IF ({srcStr()}==0) PC += {inst['offset']}"
 
 
   elif inst["opcode"]=="NOP":
@@ -44,33 +44,33 @@ def imemToStrList(imem):
       if inst["dest"]==0:
         return " _"
       else:
-        return f"r{inst["dest"]}"
+        return f"r{inst['dest']}"
     def srcStr():
       if inst["src"]==7:
         return "rSec"
       else:
-        return f"r{inst["src"]}"
+        return f"r{inst['src']}"
 
 
     if   inst["opcode"]=="ALU":
-      strList[i] += f"{destStr()} <- ALU-{inst["port"]}(r{inst["src"]}, lat={inst["latency"]}) // {inst["name"]}"
+      strList[i] += f"{destStr()} <- ALU-{inst['port']}(r{inst['src']}, lat={inst['latency']}) // {inst['name']}"
 
 
     elif inst["opcode"]=="LOAD":
       if "src" in inst:
-        strList[i] += f"{destStr()} <- LOAD(r{inst["src"]})         // {inst["name"]}"
+        strList[i] += f"{destStr()} <- LOAD(r{inst['src']})         // {inst['name']}"
       else:
-        strList[i] += f"{destStr()} <- LOAD(0x{inst["srcImm"]})        // {inst["name"]}"
+        strList[i] += f"{destStr()} <- LOAD(0x{inst['srcImm']})        // {inst['name']}"
 
 
     elif inst["opcode"]=="BREZ":
-      strList[i] += f"IF(r{inst["src"]}==0) PC <- PC + {inst["offset"]} // {inst["name"]}"
+      strList[i] += f"IF(r{inst['src']}==0) PC <- PC + {inst['offset']} // {inst['name']}"
       for j in range(i+1, min(i+inst["offset"], len(imem))):
         strList[j] += "  "
 
 
     elif inst["opcode"]=="NOP":
-      strList[i] += f"NOP                    // {inst["name"]}"
+      strList[i] += f"NOP                    // {inst['name']}"
 
 
   return strList
